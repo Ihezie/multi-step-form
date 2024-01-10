694d2394f3af41fa2798172014db698b5b28331b
Raymond Ogbuehi <raymondogb@gmail.com>
2024-01-10 18:56:46 +0100

    final form body layout and animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import FormBody from "./components/FormBody";
 import FormSteps from "./components/FormSteps";
+import { motion } from "framer-motion";
 
 function App() {
+  const variants = {
+    show: {
+      scale: "var(--main-scale-visible)",
+      transition: {
+        duration: 0.8,
+        type: "spring",
+      },
+    },
+    hide: {
+      scale: "var(--main-scale-hidden)",
+      x: "var(--translate)",
+      y: "var(--translate)",
+    },
+  };
   return (
-    <main className="min-h-full relative lg:grid lg:w-[950px] lg:absolute lg:left-1/2 lg:top-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2 lg:grid-cols-[30%_70%] lg:min-h-0 lg:bg-white lg:p-4 lg:rounded-xl shadow-2xl shadow-light-gray">
+    <motion.main
+      variants={variants}
+      initial="hide"
+      animate="show"
+      className="min-h-full relative lg:grid lg:w-[950px] lg:absolute lg:left-1/2 lg:top-1/2 lg:[--translate:-50%] lg:grid-cols-[30%_70%] lg:min-h-0 lg:bg-white lg:p-4 lg:rounded-xl shadow-2xl shadow-light-gray lg:[--main-scale-hidden:0%] lg:[--main-scale-visible:100%]"
+    >
       <FormSteps />
       <FormBody />
-    </main>
+    </motion.main>
   );
 }
 
--- src/components/FormBody.jsx
+++ src/components/FormBody.jsx
@@ -4,23 +4,36 @@ import StepOne from "./StepOne";
 const FormBody = () => {
   const formBodyVariants = {
     show: {
-      scale: 1,
+      scale: "var(--scale-visible, 1)",
       transition: {
         duration: 0.8,
         type: "spring",
       },
     },
     hide: {
-      scale: "var(--scale-hidden)",
+      scale: "var(--scale-hidden, 1)",
+    },
+  };
+  const buttonVariants = {
+    show: {
+      scale: "var(--scale-visible, 1)",
+      transition: {
+        delay: '0.5',
+        duration: 0.5,
+        type: "spring",
+      },
+    },
+    hide: {
+      scale: "var(--scale-hidden, 1)",
     },
   };
   return (
-    <section className="sm:relative sm:w-4/5 sm:mx-auto sm:bottom-20 lg:bottom-0 lg:w-full">
+    <section className="sm:relative sm:w-4/5 sm:mx-auto sm:bottom-20 lg:bottom-0 lg:w-full lg:pl-[15%] lg:pr-[13%]">
       <motion.section
         variants={formBodyVariants}
         initial="hide"
         animate="show"
-        className="w-[90%] mx-auto bg-white shadow-2xl shadow-light-gray rounded-lg px-6 py-8 relative bottom-20 sm:w-full sm:bottom-0 sm:rounded-xl sm:px-10 sm:pt-10 sm:pb-32 lg:pb-36 lg:static lg:shadow-none lg:rounded-none lg:w-full lg:bg-transparent lg:pl-[15%] lg:pr-[13%] max-lg:[--scale-hidden:0%]"
+        className="w-[90%] mx-auto bg-white shadow-2xl shadow-light-gray rounded-lg px-6 py-8 relative bottom-20 sm:w-full sm:bottom-0 sm:rounded-xl sm:px-10 sm:pt-10 sm:pb-32 lg:pb-0 lg:mb-20 lg:px-0 lg:static lg:shadow-none lg:rounded-none lg:bg-transparent max-lg:[--scale-hidden:0%] max-lg:[--scale-visible:100%]"
       >
         <header className="mb-5 lg:mb-8">
           <h1 className="font-bold text-2xl text-marine-blue mb-3 lg:text-3xl">
@@ -32,19 +45,22 @@ const FormBody = () => {
         </header>
         <StepOne />
       </motion.section>
-      <motion.div
-        variants={formBodyVariants}
-        initial="hide"
-        animate="show"
-        className="bg-white absolute bottom-0 h-20 w-full flex items-center justify-between px-[5%] sm:bg-transparent sm:px-10 lg:pr-[13%] max-lg:[--scale-hidden:0%] origin-top"
-      >
+      <div className="bg-white absolute bottom-0 h-20 w-full flex items-center justify-between px-[5%] sm:bg-transparent sm:px-10 lg:static lg:px-0  origin-top">
         <button className="text-cool-gray font-medium cursor-pointer my-transition hover:text-marine-blue invisible">
           Go Back
         </button>
-        <button className="text-white bg-marine-blue w-28 py-3 rounded font-medium hover:bg-marine-blue/85 my-transition sm:rounded-lg">
+        <motion.button
+          variants={buttonVariants}
+          initial="hide"
+          animate="show"
+          className="text-white bg-marine-blue w-28 py-3 rounded font-medium sm:rounded-lg  max-lg:[--scale-hidden:0%] max-lg:[--scale-visible:100%]"
+          whileHover={{
+            backgroundColor: "rgba(2, 42, 90, 0.85)",
+          }}
+        >
           Next Step
-        </button>
-      </motion.div>
+        </motion.button>
+      </div>
     </section>
   );
 };
