f22d2d1ca03dbccd5ecd4b0db78c38f2f7e78577
Raymond Ogbuehi <raymondogb@gmail.com>
2024-01-10 14:57:35 +0100

    testing

--- src/components/FormBody.jsx
+++ src/components/FormBody.jsx
@@ -15,26 +15,24 @@ const FormBody = () => {
     },
   };
   return (
-    <>
-      <motion.section
-        variants={formBodyVariants}
-        initial="hide"
-        animate="show"
-        className="sm:w-4/5 sm:mx-auto sm:bg-white sm:shadow-2xl sm:shadow-light-gray sm:relative sm:bottom-20 sm:rounded-xl sm:px-10 sm:pt-10 sm:pb-5 lg:static lg:bottom-0 lg:shadow-none lg:rounded-none lg:w-full lg:bg-transparent lg:pl-[15%] lg:pr-[13%] lg:pb-0 max-lg:[--scale-hidden:0%]"
-      >
-        <section className="w-[90%] mx-auto bg-white shadow-2xl shadow-light-gray rounded-lg px-6 py-8 relative bottom-20 sm:w-full sm:mx-0 sm:shadow-none sm:static sm:bottom-0 sm:rounded-none sm:bg-transparent sm:mb-16 sm:p-0 lg:mb-20">
-          <header className="mb-5 lg:mb-8">
-            <h1 className="font-bold text-2xl text-marine-blue mb-3 lg:text-3xl">
-              Personal info
-            </h1>
-            <h2 className="text-cool-gray">
-              Please provide your name, email address, and phone number.
-            </h2>
-          </header>
-          <StepOne />
-        </section>
-      </motion.section>
-      <div className="bg-white absolute bottom-0 h-20 w-full flex items-center justify-between px-[5%] sm:bg-transparent sm:w-4/5 sm:left-1/2 sm:-translate-x-1/2 sm:px-0 sm:bottom-4 0">
+    <motion.section
+      variants={formBodyVariants}
+      initial="hide"
+      animate="show"
+      className="sm:w-4/5 sm:mx-auto sm:bg-white sm:shadow-2xl sm:shadow-light-gray sm:relative sm:bottom-20 sm:rounded-xl sm:px-10 sm:pt-10 sm:pb-5 lg:static lg:bottom-0 lg:shadow-none lg:rounded-none lg:w-full lg:bg-transparent lg:pl-[15%] lg:pr-[13%] lg:pb-0 max-lg:[--scale-hidden:0%]"
+    >
+      <section className="w-[90%] mx-auto bg-white shadow-2xl shadow-light-gray rounded-lg px-6 py-8 relative bottom-20 sm:w-full sm:mx-0 sm:shadow-none sm:static sm:bottom-0 sm:rounded-none sm:bg-transparent sm:mb-16 sm:p-0 lg:mb-20">
+        <header className="mb-5 lg:mb-8">
+          <h1 className="font-bold text-2xl text-marine-blue mb-3 lg:text-3xl">
+            Personal info
+          </h1>
+          <h2 className="text-cool-gray">
+            Please provide your name, email address, and phone number.
+          </h2>
+        </header>
+        <StepOne />
+      </section>
+      <div className="bg-white absolute bottom-0 h-20 w-full flex items-center justify-between px-[5%] sm:bg-transparent sm:static sm:px-0">
         <button className="text-cool-gray font-medium cursor-pointer my-transition hover:text-marine-blue invisible">
           Go Back
         </button>
@@ -42,7 +40,7 @@ const FormBody = () => {
           Next Step
         </button>
       </div>
-    </>
+    </motion.section>
   );
 };
 export default FormBody;
